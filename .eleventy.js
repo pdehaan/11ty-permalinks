14217cb550a701589bddeb464b14f2fe7554e903
Peter deHaan <peter@deseloper.com>
2021-01-18 07:46:48 -0800

    Add pathPrefix and custom filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,15 @@
 module.exports = (eleventyConfig) => {
-  eleventyConfig.addFilter("stringify", obj => JSON.stringify(obj, null, 2));
+  eleventyConfig.addFilter("stringify", (obj) => JSON.stringify(obj, null, 2));
+  eleventyConfig.addFilter("stripLeadingSlash", (urlPath = "") =>
+    urlPath.startsWith("/") ? urlPath.slice(1) : urlPath
+  );
   eleventyConfig.addLayoutAlias("base", "layouts/base.html");
 
   return {
     dir: {
       input: "src",
       output: "www",
-    }
+    },
+    pathPrefix: "/~pdehaan/",
   };
 };
